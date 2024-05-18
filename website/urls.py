3ccc9b8142deae632f39c3520ce9e11c642bef9d
navid <n.ahmadi.livani@gmail.com>
2024-05-18 20:00:18 +0330

    2
    2

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,8 @@ from website.views import *
 
 urlpatterns = [
 
-    path('http-test', http_test),
+    path('',index_view),
+    path('about',about_view),
+    path('contact',contact_view),
+
 ]
--- website/views.py
+++ website/views.py
@@ -5,5 +5,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def http_test(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def index_view(request):
+    return render(request, 'website/index.html')
+
+
+def about_view(request):
+    return render(request, 'website/about.html')
+
+
+def contact_view(request):
+    return render(request, 'website/contact.html')
